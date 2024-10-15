821398dba23b0fc553fd8f01d44aad7a293d0439
srimani645 <srimani645@gmail.com>
2024-10-15 20:12:06 +0530

    Update tables.blade.php

--- resources/views/home/tables.blade.php
+++ resources/views/home/tables.blade.php
@@ -5,7 +5,9 @@
 
 @section('mainbodycontent')
 <div class="container">
-    <h2 class="my-4 text-center">Total Login Table</h2>
+    <div class="row">
+        <div class="col-lg-5">
+
     <div class="card shadow-lg mb-4"> <!-- Card wrapper -->
         <div class="card-header text-white bg-primary">
             <h5 class="mb-0">Total Login Records</h5>
@@ -15,33 +17,56 @@
                 <thead>
                     <tr>
                         <th scope="col">Id</th>
-                        <th scope="col">Name</th>
+                        
                         <th scope="col">Email</th>
-                        <th scope="col">Total Login</th>
+                        <th scope="col">Login</th>
                     </tr>
                 </thead>
                 <tbody>
+                    @foreach($totalLogins as $totallogin)
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>mark@gmail.com</td>
                         <td>1</td>
+                        <td>{{ $totallogin->email }}</td>
+                        <td>{{ $totallogin->Total_Login }}</td>
+                        
                     </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>jack@gail.com</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>larry@gmail.com</td>
-                        <td>3</td>
-                    </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
+</div>
+<div class="col-lg-7">
+
+    <div class="card shadow-lg mb-4"> <!-- Card wrapper -->
+        <div class="card-header text-white bg-primary">
+            <h5 class="mb-0">Total Login Records</h5>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                       
+                        <th scope="col">Email</th>
+                        <th scope="col">Month</th>
+                        <th scope="col">Logincount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($month_wise_login_count as $mwtl)
+                    <tr>
+                       
+                        <td>{{ $mwtl->email }}</td>
+                        <td>{{ $mwtl->Month_Name }}</td>
+                        <td>{{ $mwtl->count }}</td>
+                        
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
 </div>
 @endsection
